c718aa2df676d21f518761fa7fd7996d36aec7b5
Atin Agnihotri <atinjiagnihotri@gmail.com>
2023-09-15 23:28:40 +0530

    metrics middleware

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
+
+type apiConfig struct {
+	fileServerHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsIncrement(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits += 1
+		next.ServeHTTP(w, r)
+	})
+}
 
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -19,17 +31,34 @@ func corsMiddleware(next http.Handler) http.Handler {
 }
 
 func main() {
+	cfg := apiConfig{}
 	mux := http.NewServeMux()
 	port := "8080"
 	fileDir := http.Dir(".")
+
+	// health endpoint
 	mux.Handle("/healthz", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.Write([]byte("OK"))
 		return
 	}))
-	// mux.Handle("/app", http.FileServer(fileDir))
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(fileDir)))
+
+	// metrics endpoints
+	mux.Handle("/metrics", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileServerHits)))
+		return
+	}))
+	mux.Handle("/reset", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		cfg.fileServerHits = 0
+		return
+	}))
+
+	// fileserver endpoint
+	mux.Handle("/app/", cfg.middlewareMetricsIncrement(http.StripPrefix("/app", http.FileServer(fileDir))))
+
 	corsMux := corsMiddleware(mux)
 
 	srv := &http.Server{
